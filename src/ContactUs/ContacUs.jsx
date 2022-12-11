a1879be62c70e2d99335ee703cb450354796a6ef
jorge <jorgebarrera680@gmail.com>
2022-12-11 11:26:33 -0600

    html updated

--- src/ContactUs/ContacUs.jsx
+++ src/ContactUs/ContacUs.jsx
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./ContactUs.css">
-  <title>Document</title>
-</head>
+const React = require('react')
+
+
+const Def = () => {
+  return(
+
+
+  
+ 
 <body>
   
   <div class="container">
@@ -46,6 +46,9 @@
         </div>
     </div>
   </div>
-  
-</body>
-</html>
+  </body>
+
+)
+}
+
+module.exports = Def
