19bfbeb86e66bf1a439991ad970455c40f7ee382
cfdebug <rfadventure22@gmail.com>
2022-12-13 21:42:56 -0600

    Fixed Merge

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
-
 const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
