9de20a55cb0f8388b0a991c586647293093733a2
cfdebug <rfadventure22@gmail.com>
2022-12-14 21:50:09 -0600

    Fixed Mongo Connection

--- server/models/dbConfig.js
+++ server/models/dbConfig.js
@@ -1,37 +1,38 @@
 // require mongoose
-const mongoose = require("mongoose");
+const mongoose= require("mongoose");
 // creating shorthand for the Schema constructor
 const { Schema } = mongoose
 
+// wishlist schema
 
-// links child schema
 const linkSchema = new Schema({
     goat: String,
     flightClub: String,
     stadiumGoods: String
 })
-const Link = mongoose.model('Link', linkSchema)
 
-// wish schema
+const imageSchema = new Schema({
+    360: Array,
+    original: String,
+    small: String,
+    thumbnail: String
+})
+
 const wishSchema = new Schema({
     brand: String,
     colorway: String,
     estimatedMarketValue: Number,
     gender: String,
-    image: String,
-    link: {
-        type:{Link}
-    },
+    image: { type: imageSchema },
+    links: { type: linkSchema },
     name: String,
     releaseDate: String,
     releaseYear: String,
     retailPrice: Number,
     silhouette: String,
     sku: String,
-    story: String
+    story: String 
 })
 
-const Wish = mongoose.model('Wish', wishSchema)
-// const doc = await Wish.findOne().populate('link');
-// doc.child
-module.exports = Wish
+// const Link = mongoose.model('Link', linkSchema)
+module.exports = mongoose.model('Wish', wishSchema)
